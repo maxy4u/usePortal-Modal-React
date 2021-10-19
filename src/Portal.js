e86b1ba652003cc1da34d1b5070871259ade01d8
maxy4u <khurana.g@hotmail.com>
2021-10-19 14:11:21 -0300

    1 file modified

--- src/Portal.js
+++ src/Portal.js
@@ -2,12 +2,6 @@ import React, { memo } from 'react';
 import { createPortal } from 'react-dom';
 import usePortal from './usePortal';
 
-/**
- * @example
- * <Portal id="modal">
- *   <p>Thinking with portals</p>
- * </Portal>
- */
 const Portal = ({ id, children }) => {
   const target = usePortal(id);
   return createPortal(children, target);
